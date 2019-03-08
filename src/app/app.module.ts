f0d4d5a2d5445d8caae3b1011978b6c501daecea
muhammed alagoz <muhammed.alagoz@bil.omu.edu.tr>
2019-03-08 11:24:26 +0300

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
